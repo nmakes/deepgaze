ca9b43672b0e0472b81279a58a9e1a2ef969221e
Massimiliano Patacchiola <massimiliano.patacchiola@gmail.com>
2016-05-03 21:45:21 +0100

    Bayes filter bug fix in update and prediction

---
+++ ex_bayes_filter.py
@@ -0,0 +1,75 @@
+from include.bayes_filter import DiscreteBayesFilter
+import numpy as np
+
+
+my_filter = DiscreteBayesFilter(10)
+
+belief = np.array([1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0], dtype=np.float32)
+
+cpt_motion_model = np.array([[0.8, 0.1, 0, 0, 0, 0, 0, 0, 0, 0.1],
+                             [0.1, 0.8, 0.1, 0, 0, 0, 0, 0, 0, 0],
+                             [0, 0.1, 0.8, 0.1, 0, 0, 0, 0, 0, 0],
+                             [0, 0, 0.1, 0.8, 0.1, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0.1, 0.8, 0.1, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0.1, 0.8, 0.1, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0.1, 0.8, 0.1, 0, 0],
+                             [0, 0, 0, 0, 0, 0, 0.1, 0.8, 0.1, 0],
+                             [0, 0, 0, 0, 0, 0, 0, 0.1, 0.8, 0.1],
+                             [0.1, 0, 0, 0, 0, 0, 0, 0, 0.1, 0.8]], dtype=np.float32)
+
+cpt_measurement_accuracy = np.array([[0.6, 0.2, 0.1, 0.1, 0, 0, 0, 0, 0, 0],
+                                     [0.1, 0.8, 0.1, 0, 0, 0, 0, 0, 0, 0],
+                                     [0, 0.1, 0.8, 0.1, 0, 0, 0, 0, 0, 0],
+                                     [0, 0.1, 0.3, 0.3, 0.3, 0, 0, 0, 0, 0],
+                                     [0, 0, 0, 0.1, 0.8, 0.1, 0, 0, 0, 0],
+                                     [0, 0, 0, 0, 0.1, 0.8, 0.1, 0, 0, 0],
+                                     [0, 0, 0, 0, 0, 0.1, 0.8, 0.1, 0, 0],
+                                     [0, 0, 0, 0, 0, 0, 0.1, 0.8, 0.1, 0],
+                                     [0, 0, 0, 0, 0, 0, 0, 0.1, 0.8, 0.1],
+                                     [0, 0, 0, 0, 0, 0, 0, 0.1, 0.1, 0.8]], dtype=np.float32)
+
+print("From unknown position moving to 1")
+belief = np.array([0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2], dtype=np.float32)
+
+for i in range(10):
+    belief_predicted = my_filter.predict(belief, cpt_motion_model)
+    belief_updated = my_filter.update(belief_predicted, 1, cpt_measurement_accuracy)
+    belief = belief_updated
+    print("Estimated state: " + str(np.argmax(belief)) + " with probability: " + str(belief[np.argmax(belief)]))
+
+print("Moving to state 2")
+for i in range(10):
+    belief_predicted = my_filter.predict(belief, cpt_motion_model)
+    belief_updated = my_filter.update(belief_predicted, 2, cpt_measurement_accuracy)
+    belief = belief_updated
+    print("Estimated state: " + str(np.argmax(belief)) + " with probability: " + str(belief[np.argmax(belief)]))
+
+print("Moving to state 3")
+for i in range(10):
+    belief_predicted = my_filter.predict(belief, cpt_motion_model)
+    belief_updated = my_filter.update(belief_predicted, 3, cpt_measurement_accuracy)
+    belief = belief_updated
+    print("Estimated state: " + str(np.argmax(belief)) + " with probability: " + str(belief[np.argmax(belief)]))
+
+print("Some dirty measures around 3...")
+for i in range(3):
+    belief_predicted = my_filter.predict(belief, cpt_motion_model)
+    belief_updated = my_filter.update(belief_predicted, 2, cpt_measurement_accuracy)
+    belief = belief_updated
+    print("Estimated state: " + str(np.argmax(belief)) + " with probability: " + str(belief[np.argmax(belief)]))
+    belief_predicted = my_filter.predict(belief, cpt_motion_model)
+    belief_updated = my_filter.update(belief_predicted, 3, cpt_measurement_accuracy)
+    belief = belief_updated
+    print("Estimated state: " + str(np.argmax(belief)) + " with probability: " + str(belief[np.argmax(belief)]))
+    belief_predicted = my_filter.predict(belief, cpt_motion_model)
+    belief_updated = my_filter.update(belief_predicted, 4, cpt_measurement_accuracy)
+    belief = belief_updated
+    print("Estimated state: " + str(np.argmax(belief)) + " with probability: " + str(belief[np.argmax(belief)]))
+
+
+
+
+
+
+
+
--- include/bayes_filter.py
+++ include/bayes_filter.py
@@ -47,32 +47,32 @@ class DiscreteBayesFilter:
             self._prior = prior.copy()
             self._cpt = cpt.copy()
 
+
     ##
     # After the initialisation it is possible to predict the current value
-    # of the quanity X. It is necessary to pass the cpt and the prior in the
-    # initialisation phase. After the prediction this function update the 
-    # internal prior with the posterior just computed. Basically we are 
-    # applying the Bayes theorem to estimate the posterior.
-    # @param evidence is a scalar, it represents the observed state of Z
+    # of the quanity X. It is necessary to pass the cpt_motion_model and the
+    # belief.
+    #
+    # @param cpt_motion_model is the conditional probability table of
+    # x(t)|x(t-1). The control u is not used in our case
+    # @param belief is the probability distribution vector associated with x
     # @return it returns the posterior distribution of X given the evidence
-    def predict_and_update(self, evidence):
+    def predict(self, belief, cpt_motion_model):
+        belief = np.matrix(belief) * cpt_motion_model
+        return np.asarray(belief).reshape(-1)
+
+
+    def update(self, belief_predicted, measure, cpt_measure_accuracy):
         #Getting P(Z) and Likelihood
-        p_z = np.sum(self._cpt[:,evidence]) #scalar P(Z)
-        likelihood = self._cpt[:,evidence].copy() #vector P(Z|X)
+        p_z = np.sum(cpt_measure_accuracy[:,measure]) #scalar P(Z)
+        likelihood = cpt_measure_accuracy[:,measure].copy() #vector P(Z|X)
         #likelihood = np.transpose(likelihood)
         #Getting the posterior distribution
-        posterior = np.multiply(self._prior, likelihood)
-        posterior /= p_z #vector P(X|Z)
+        belief_updated = np.multiply(belief_predicted, likelihood)
+        belief_updated /= p_z #vector P(X|Z)
         #Normalise to sum up to 1
-        normalisation = np.sum(posterior)
-        posterior /= normalisation
-        #Update the posterio with the new value
-        self._prior = posterior
-        return posterior.copy()
-
-
-
-
-
+        normalisation = np.sum(belief_updated)
+        belief_updated /= normalisation
+        return belief_updated
 
     
