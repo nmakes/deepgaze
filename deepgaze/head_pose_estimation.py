cb07c5046d2fa03417752978dc4025dd927a1294
Massimiliano Patacchiola <massimiliano.patacchiola@gmail.com>
2016-10-03 16:53:09 +0100

    added the rotationMatrixToEulerAngles function

--- deepgaze/head_pose_estimation.py
+++ deepgaze/head_pose_estimation.py
@@ -13,6 +13,7 @@ import tensorflow as tf
 import cv2
 import os.path
 import imp #to check for missing modules
+import math
 
 #Check if dlib is installed
 try:
@@ -267,46 +268,30 @@ class PnpHeadPoseEstimator:
 
         if(DEBUG==True): print("[DEEPGAZE] PnpHeadPoseEstimator: estimated camera matrix: \n" + str(self.camera_matrix) + "\n")
 
-
-        #This matrix contains the 3D points of the
-        # 11 landmarks we want to find. It has been
-        # obtained from antrophometric measurement
-        # of the human head.
-        self.landmarks_3D = np.float32([P3D_RIGHT_SIDE,
-                                        P3D_GONION_RIGHT,
-                                        P3D_MENTON,
-                                        P3D_GONION_LEFT,
-                                        P3D_LEFT_SIDE,
-                                        P3D_FRONTAL_BREADTH_RIGHT,
-                                        P3D_FRONTAL_BREADTH_LEFT,
-                                        P3D_SELLION,
-                                        P3D_NOSE,
-                                        P3D_SUB_NOSE,
-                                        P3D_RIGHT_EYE,
-                                        P3D_RIGHT_TEAR,
-                                        P3D_LEFT_TEAR,
-                                        P3D_LEFT_EYE,
-                                        P3D_STOMION])
-
         #Declaring the dlib shape predictor object
         self._shape_predictor = dlib.shape_predictor(dlib_shape_predictor_file_path)
 
 
     def _return_landmarks(self, inputImg, roiX, roiY, roiW, roiH, points_to_return=range(0,68)):
-            #Creating a dlib rectangle and finding the landmarks
-            dlib_rectangle = dlib.rectangle(left=int(roiX), top=int(roiY), right=int(roiW), bottom=int(roiH))
-            dlib_landmarks = self._shape_predictor(inputImg, dlib_rectangle)
+        """ Return the the roll pitch and yaw angles associated with the input image.
 
-            #It selects only the landmarks that
-            #have been indicated in the input parameter "points_to_return".
-            #It can be used in solvePnP() to estimate the 3D pose.
-            landmarks = np.zeros((len(points_to_return),2), dtype=np.float32)
-            counter = 0
-            for point in points_to_return:
-                landmarks[counter] = [dlib_landmarks.parts()[point].x, dlib_landmarks.parts()[point].y]
-                counter += 1
+        @param image It is a colour image. It must be >= 64 pixel.
+        @param radians When True it returns the angle in radians, otherwise in degrees.
+        """
+        #Creating a dlib rectangle and finding the landmarks
+        dlib_rectangle = dlib.rectangle(left=int(roiX), top=int(roiY), right=int(roiW), bottom=int(roiH))
+        dlib_landmarks = self._shape_predictor(inputImg, dlib_rectangle)
 
-            return landmarks
+        #It selects only the landmarks that
+        #have been indicated in the input parameter "points_to_return".
+        #It can be used in solvePnP() to estimate the 3D pose.
+        landmarks = np.zeros((len(points_to_return),2), dtype=np.float32)
+        counter = 0
+        for point in points_to_return:
+            landmarks[counter] = [dlib_landmarks.parts()[point].x, dlib_landmarks.parts()[point].y]
+            counter += 1
+
+        return landmarks
 
     def return_roll_pitch_yaw(self, image, radians=False):
          """ Return the the roll pitch and yaw angles associated with the input image.
@@ -345,6 +330,10 @@ class PnpHeadPoseEstimator:
          #P3D_LIP_LEFT = np.float32([-20.0, 65.5,-5.0]) #54
          P3D_STOMION = np.float32([10.0, 0.0, -75.0]) #62
 
+         #This matrix contains the 3D points of the
+         # 11 landmarks we want to find. It has been
+         # obtained from antrophometric measurement
+         # of the human head.
          landmarks_3D = np.float32([P3D_RIGHT_SIDE,
                                   P3D_GONION_RIGHT,
                                   P3D_MENTON,
@@ -363,7 +352,7 @@ class PnpHeadPoseEstimator:
 
          #Return the 2D position of our landmarks
          img_h, img_w, img_d = image.shape
-         landmarks_2D = my_detector.returnLandmarks(inputImg=image, roiX=0, roiY=img_w, roiW=img_w, roiH=img_h, points_to_return=TRACKED_POINTS)
+         landmarks_2D = self._return_landmarks(inputImg=image, roiX=0, roiY=img_w, roiW=img_w, roiH=img_h, points_to_return=TRACKED_POINTS)
 
          #Print som red dots on the image       
          #for point in landmarks_2D:
@@ -379,25 +368,47 @@ class PnpHeadPoseEstimator:
          #tvec - Output translation vector. It is the position of the world origin (SELLION) in camera co-ords
          retval, rvec, tvec = cv2.solvePnP(landmarks_3D, 
                                            landmarks_2D, 
-                                           camera_matrix, 
-                                           camera_distortion)
+                                           self.camera_matrix, 
+                                           self.camera_distortion)
 
-        #Get as input the rotational vector
-        #Return a rotational matrix
-        rmat, _ = cv2.Rodrigues(rvec) 
+         #Get as input the rotational vector
+         #Return a rotational matrix
+         rmat, _ = cv2.Rodrigues(rvec) 
 
-        #euler_angles contain (pitch, yaw, roll)
-        #euler_angles = cv.DecomposeProjectionMatrix(projMatrix=rmat, cameraMatrix=camera_matrix, rotMatrix, transVect, rotMatrX=None, rotMatrY=None, rotMatrZ=None)
+         #euler_angles contain (pitch, yaw, roll)
+         #euler_angles = cv.DecomposeProjectionMatrix(projMatrix=rmat, cameraMatrix=camera_matrix, rotMatrix, transVect, rotMatrX=None, rotMatrY=None, rotMatrZ=None)
 
-        head_pose = [ rmat[0,0], rmat[0,1], rmat[0,2], tvec[0],
-                      rmat[1,0], rmat[1,1], rmat[1,2], tvec[1],
-                      rmat[2,0], rmat[2,1], rmat[2,2], tvec[2],
-                            0.0,      0.0,        0.0,    1.0 ]
+         head_pose = [ rmat[0,0], rmat[0,1], rmat[0,2], tvec[0],
+                       rmat[1,0], rmat[1,1], rmat[1,2], tvec[1],
+                       rmat[2,0], rmat[2,1], rmat[2,2], tvec[2],
+                             0.0,      0.0,        0.0,    1.0 ]
+         print(head_pose) #TODO remove this line
+         return self.rotationMatrixToEulerAngles(rmat)
 
 
 
-
-
+    # Calculates rotation matrix to euler angles
+    # The result is the same as MATLAB except the order
+    # of the euler angles ( x and z are swapped ).
+    def rotationMatrixToEulerAngles(self, R) :
+ 
+        #assert(isRotationMatrix(R))
+     
+        #To prevent the Gimbal Lock it is possible to use
+        #a threshold of 1e-6 for discrimination
+        sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])    
+        singular = sy < 1e-6
+ 
+        if  not singular :
+            x = math.atan2(R[2,1] , R[2,2])
+            y = math.atan2(-R[2,0], sy)
+            z = math.atan2(R[1,0], R[0,0])
+        else :
+            x = math.atan2(-R[1,2], R[1,1])
+            y = math.atan2(-R[2,0], sy)
+            z = 0
+ 
+        return np.array([x, y, z])
 
 
 
