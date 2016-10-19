d9456085d3374ac78a173de4ca605805f12ffe30
Massimiliano Patacchiola <massimiliano.patacchiola@gmail.com>
2016-10-19 14:38:46 +0100

    Adding the example for pitch estimation with CNN

---
+++ examples/ex_cnn_headp_pose_estimation_images/ex_cnn_head_pose_estimation_images_pitch.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+#The MIT License (MIT)
+#Copyright (c) 2016 Massimiliano Patacchiola
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+#MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+#CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+#SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import tensorflow as tf
+import cv2
+from deepgaze.head_pose_estimation import CnnHeadPoseEstimator
+
+sess = tf.Session() #Launch the graph in a session.
+my_head_pose_estimator = CnnHeadPoseEstimator(sess) #Head pose estimation object
+my_head_pose_estimator.load_pitch_variables("../../etc/tensorflow/head_pose/pitch/cnn_arch2_pitch_f2-29999")
+
+for i in range(1,9):
+    file_name = str(i) + ".jpg"
+    print("Processing image ..... " + file_name)
+    image = cv2.imread(file_name) #Read the image with OpenCV
+    pitch = my_head_pose_estimator.return_pitch(image) #Evaluate the yaw angle using a CNN
+    print("Estimated pitch ..... " + str(pitch[0,0,0]))
+    print("")
