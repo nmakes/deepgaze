2af63e07960b51f575443cd32a92c7357b35eb9b
Massimiliano Patacchiola <massimiliano.patacchiola@gmail.com>
2016-10-27 11:42:03 +0100

    Updated the example using the new class mask analysis

--- examples/ex_face_center_color_detection/ex_face_center_color_detection.py
+++ examples/ex_face_center_color_detection/ex_face_center_color_detection.py
@@ -17,11 +17,13 @@
 import numpy as np
 import cv2
 from deepgaze.color_detection import RangeColorDetector
+from deepgaze.mask_analysis import BinaryMaskAnalyser
 
 #Declaring the boundaries and creating the object
 min_range = np.array([0, 58, 50], dtype = "uint8") #lower HSV boundary of skin color
 max_range = np.array([30, 255, 255], dtype = "uint8") #upper HSV boundary of skin color
 my_skin_detector = RangeColorDetector(min_range, max_range) #Define the detector object
+my_mask_analyser = BinaryMaskAnalyser()
 
 image = cv2.imread("tomb_rider_2.jpg") #Read the image with OpenCV
 #For this image we use one iteration of the morph_opening and gaussian blur to clear the noise
@@ -31,9 +33,10 @@ image_mask = my_skin_detector.returnMask(image, morph_opening=True, blur=True, k
 
 #Here we get the center of the contour with largest area and
 #the contour points.
-cx, cy = my_skin_detector.returnMaxAreaCenter(image_mask)
-cnt = my_skin_detector.returnMaxAreaContour(image_mask)
-#Uncomment if you want to get the coords of the rectangle sourrinding
+cx, cy = my_mask_analyser.returnMaxAreaCenter(image_mask)
+cnt = my_mask_analyser.returnMaxAreaContour(image_mask)
+
+#Uncomment if you want to get the coords of the rectangle sorrounding
 #the largest area contour (in this case the face)
 #x, y, w, h = my_skin_detector.returnMaxAreaRectangle(image_mask)
 
