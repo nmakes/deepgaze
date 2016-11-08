bab1a31ecc1c6fe3112af15f4145a213ea0cd671
Massimiliano Patacchiola <massimiliano.patacchiola@gmail.com>
2016-11-08 19:16:23 +0000

    Added the MultiBackProjectionColorDetector class

--- deepgaze/color_detection.py
+++ deepgaze/color_detection.py
@@ -100,6 +100,96 @@ class BackProjectionColorDetector:
         #Merge the threshold matrices
         return frame_threshold
 
+class MultiBackProjectionColorDetector:
+    """Implementation of the Histogram Backprojection algorithm with multi-template.
+
+    This class is the reimplementation of the BackProjectionColorDetector class for
+    multi-template color detection. Instead of specifing a single template it is 
+    possible to pass a list of templates, which can be multiple subframe taken from
+    different part of an object. Multiple version of the Backprojection algorithm
+    are then run at the same time and the filtered output added togheter. The result
+    of this process is much robust (but slower) than the standard class.
+    """
+
+    def __init__(self):
+        """Init the color detector object.
+
+    """
+        self.template_hsv_list = list()
+
+    def setTemplateList(self, frame_list):
+        """Set the BGR image list used as container for the templates
+ 
+        The template can be a spedific region of interest of the main
+        frame or a representative color scheme to identify. the template
+        is internally stored as an HSV image.
+        @param frame the template to use in the algorithm
+        """ 
+        for frame in frame_list:    
+            self.template_hsv_list.append(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV))
+
+    def getTemplateList(self):
+        """Get the BGR image list used as container for the templates
+ 
+        The template can be a spedific region of interest of the main
+        frame or a representative color scheme to identify.
+        """
+        output_list = list()
+        for frame in self.template_hsv_list:
+            output_list.append(cv2.cvtColor(frame, cv2.COLOR_HSV2BGR))
+        return output_list
+
+    def returnFiltered(self, frame, morph_opening=True, blur=True, kernel_size=5, iterations=1):
+        """Given an input frame in BGR return the filtered version.
+ 
+        @param frame the original frame (color)
+        @param morph_opening it is a erosion followed by dilatation to remove noise
+        @param blur to smoth the image it is possible to apply Gaussian Blur
+        @param kernel_size is the kernel dimension used for morph and blur
+        """
+        if(len(self.template_hsv_list) == 0): return None
+        #Get the mask from the internal function
+        frame_threshold = self.returnMask(frame, morph_opening=morph_opening, blur=blur, kernel_size=kernel_siz, iterations=iterations)
+        #Return the AND image
+        return cv2.bitwise_and(frame, frame_threshold)
+
+    def returnMask(self, frame, morph_opening=True, blur=True, kernel_size=5, iterations=1):
+        """Given an input frame in BGR return the black/white mask.
+ 
+        @param frame the original frame (color)
+        @param morph_opening it is a erosion followed by dilatation to remove noise
+        @param blur to smoth the image it is possible to apply Gaussian Blur
+        @param kernel_size is the kernel dimension used for morph and blur
+        """
+        if(len(self.template_hsv_list) == 0): return None
+        frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask = np.zeros((frame.shape[0], frame.shape[1]))
+        for template_hsv in self.template_hsv_list:
+            #Set the template histogram
+            template_hist = cv2.calcHist([template_hsv],[0, 1], None, [180, 256], [0, 180, 0, 256] )
+            #Normalize the template histogram and apply backprojection
+            cv2.normalize(template_hist, template_hist, 0, 255, cv2.NORM_MINMAX)
+            frame_hsv_back = cv2.calcBackProject([frame_hsv], [0,1], template_hist, [0,180,0,256], 1)
+            #Get the kernel and apply a convolution
+            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel_size,kernel_size))
+            frame_hsv_clean = cv2.filter2D(frame_hsv_back, -1, kernel)
+            #Applying the morph open operation (erosion followed by dilation)
+            if(morph_opening==True):
+                kernel = np.ones((kernel_size,kernel_size), np.uint8)
+                frame_hsv_clean = cv2.morphologyEx(frame_hsv_clean, cv2.MORPH_OPEN, kernel, iterations=iterations)
+            #Applying Gaussian Blur
+            if(blur==True): 
+                frame_hsv_clean = cv2.GaussianBlur(frame_hsv_clean, (kernel_size,kernel_size), 0)
+            #Get the threshold
+            ret, frame_hsv_threshold = cv2.threshold(frame_hsv_clean, 50, 255, 0)
+            mask = np.add(mask, frame_hsv_threshold) #Add the threshold to the mask
+
+        #Normalize the mask because it contains
+        #values added during the previous loop
+        cv2.normalize(mask, mask, 0, 255, cv2.NORM_MINMAX)
+        ret, mask = cv2.threshold(mask.astype(np.uint8), 50, 255, 0)
+        return mask.astype(np.uint8)
+
 class RangeColorDetector:
     """Using this detector it is possible to isolate colors in a specified range.
 
