b0109da1f91e28f05811b946df5e8e0dcc1d83fb
Massimiliano Patacchiola <massimiliano.patacchiola@gmail.com>
2016-11-10 10:04:53 +0000

    Bug correction, kernel_siz > kernel_size

--- deepgaze/color_detection.py
+++ deepgaze/color_detection.py
@@ -65,7 +65,7 @@ class BackProjectionColorDetector:
         """
         if(self.template_hsv is None): return None
         #Get the mask from the internal function
-        frame_threshold = self.returnMask(frame, morph_opening=morph_opening, blur=blur, kernel_size=kernel_siz, iterations=iterations)
+        frame_threshold = self.returnMask(frame, morph_opening=morph_opening, blur=blur, kernel_size=kernel_size, iterations=iterations)
         #Return the AND image
         return cv2.bitwise_and(frame, frame_threshold)
 
