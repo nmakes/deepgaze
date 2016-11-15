dcdb6055f832faa7685ec8e402fc118fe2041cc6
Massimiliano Patacchiola <massimiliano.patacchiola@gmail.com>
2016-11-15 13:02:24 +0000

    Possibility to convert to HSV and GRAY

--- deepgaze/color_classification.py
+++ deepgaze/color_classification.py
@@ -29,13 +29,13 @@ class HistogramColorClassifier:
     called Histogram Backprojection performs this task efficiently in crowded scenes.
     """
 
-    def __init__(self, channels=[0, 1, 2], hist_size=[8, 8, 8], bins_range=[0, 256, 0, 256, 0, 256]):
+    def __init__(self, channels=[0, 1, 2], hist_size=[10, 10, 10], hist_range=[0, 256, 0, 256, 0, 256], hist_type='BGR'):
         """Init the classifier.
 
         This class has an internal list containing all the models.
-        it is possible to append new models. Using the default values:
+        it is possible to append new models. Using the default values
         it extracts a 3D BGR color histogram from the image, using
-	8 bins per channel.
+	10 bins per channel.
         @param channels list where we specify the index of the channel 
            we want to compute a histogram for. For a grayscale image, 
            the list would be [0]. For all three (red, green, blue) channels, 
@@ -43,10 +43,20 @@ class HistogramColorClassifier:
         @param hist_size number of bins we want to use when computing a histogram. 
             It is a list (one value for each channel). Note: the bin sizes can 
             be different for each channel.
+        @param hist_range it is the min-max value of the values stored in the histogram.
+            For three channels can be [0, 256, 0, 256, 0, 256], if there is only one
+            channel can be [0, 256]
+        @param hsv_type Convert the input BGR frame in HSV or GRAYSCALE. before taking 
+            the histogram. The HSV representation can get more reliable results in 
+            situations where light have a strong influence.
+            BGR: (default) do not convert the input frame
+            HSV: convert in HSV represantation
+            GRAY: convert in grayscale
         """
         self.channels = channels
         self.hist_size = hist_size
-        self.bins_range = bins_range
+        self.hist_range = hist_range
+        self.hist_type = hist_type
         self.model_list = list()
 
     def addModelHistogram(self, model_frame, index=-1):
@@ -56,7 +66,9 @@ class HistogramColorClassifier:
             is obtained and saved in internal list.
         @param index [not implemented]
         """
-        hist = cv2.calcHist([model_frame], self.channels, None, self.hist_size, self.bins_range)
+        if(self.hist_type=='HSV'): model_frame = cv2.cvtColor(model_frame, cv2.COLOR_BGR2HSV)
+        elif(self.hist_type=='GRAY'): model_frame = cv2.cvtColor(model_frame, cv2.COLOR_BGR2GRAY)
+        hist = cv2.calcHist([model_frame], self.channels, None, self.hist_size, self.hist_range)
         hist = cv2.normalize(hist).flatten()
         self.model_list.append(hist)
 
@@ -86,8 +98,10 @@ class HistogramColorClassifier:
             intersection: (default) the histogram intersection (Swain, Ballard)
         @return a numpy array containg the comparison value between each pair image-model
         """
+        if(self.hist_type=='HSV'): image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        elif(self.hist_type=='GRAY'): image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         comparison_array = np.zeros(len(self.model_list))
-        image_hist = cv2.calcHist([image], self.channels, None, self.hist_size, self.bins_range)
+        image_hist = cv2.calcHist([image], self.channels, None, self.hist_size, self.hist_range)
         image_hist = cv2.normalize(image_hist).flatten()
         counter = 0
         for model_hist in self.model_list:
@@ -95,6 +109,20 @@ class HistogramColorClassifier:
             counter += 1
         return comparison_array
 
+    def returnHistogramComparisonProbability(self, image, method='intersection'):
+        """Return the probability distribution of the comparison between 
+        all the model and the input image. The sum of the elements in the output
+        array sum up to 1.
+
+        The highest value represents the best match.
+        @param image the image to compare
+        @param method the comparison method.
+            intersection: (default) the histogram intersection (Swain, Ballard)
+        @return a numpy array containg the comparison value between each pair image-model
+        """
+        comparison_array = self.returnHistogramComparisonArray(image=image, method=method)
+        comparison_distribution = np.divide(comparison_array, np.sum(comparison_array))
+        return comparison_distribution
 
     def returnBestMatchIndex(self, image, method='intersection'):
         """Return the index of the best match value between the image and the internal models.
