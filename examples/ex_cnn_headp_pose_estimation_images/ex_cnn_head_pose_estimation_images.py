e341b73098336928b991e3de656e0d7e2f03b7a0
默默 <wangqiang1588@sina.com>
2017-04-19 16:06:34 +0800

    Fix filename error

--- examples/ex_cnn_headp_pose_estimation_images/ex_cnn_head_pose_estimation_images.py
+++ examples/ex_cnn_headp_pose_estimation_images/ex_cnn_head_pose_estimation_images.py
@@ -8,13 +8,14 @@
 #CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
 #SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+import os
 import tensorflow as tf
 import cv2
 from deepgaze.head_pose_estimation import CnnHeadPoseEstimator
 
 sess = tf.Session() #Launch the graph in a session.
 my_head_pose_estimator = CnnHeadPoseEstimator(sess) #Head pose estimation object
-my_head_pose_estimator.load_yaw_variables("../../etc/tensorflow/head_pose/yaw/cnn_cccdd_30k.tf")
+my_head_pose_estimator.load_yaw_variables(os.path.realpath("../../etc/tensorflow/head_pose/yaw/cnn_cccdd_30k"))
 
 for i in range(1,9):
     file_name = str(i) + ".jpg"
