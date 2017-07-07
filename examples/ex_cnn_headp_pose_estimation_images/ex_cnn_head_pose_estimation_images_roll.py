a2a372fb29d654f12ee579158249c8dac2478864
Massimiliano Patacchiola <massimiliano.patacchiola@gmail.com>
2017-07-07 18:45:07 +0100

    Adding the CNN roll estimation functions

---
+++ examples/ex_cnn_headp_pose_estimation_images/ex_cnn_head_pose_estimation_images_roll.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+#The MIT License (MIT)
+#Copyright (c) 2016 Massimiliano Patacchiola
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+#MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+#CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+#SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import tensorflow as tf
+import cv2
+from deepgaze.head_pose_estimation import CnnHeadPoseEstimator
+
+sess = tf.Session() #Launch the graph in a session.
+my_head_pose_estimator = CnnHeadPoseEstimator(sess) #Head pose estimation object
+my_head_pose_estimator.load_roll_variables("../../etc/tensorflow/head_pose/roll/cnn_cccdd_30k.tf")
+
+for i in range(1,9):
+    file_name = str(i) + ".jpg"
+    print("Processing image ..... " + file_name)
+    image = cv2.imread(file_name) #Read the image with OpenCV
+    roll = my_head_pose_estimator.return_roll(image) #Evaluate the roll angle using a CNN
+    print("Estimated roll ..... " + str(roll[0,0,0]))
+    print("")
