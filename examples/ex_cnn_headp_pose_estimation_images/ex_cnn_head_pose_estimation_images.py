2b5ab25eea0f057e6a97918835f84d855a9e68eb
Massimiliano Patacchiola <massimiliano.patacchiola@gmail.com>
2017-07-07 18:52:31 +0100

    Adding the example with roll,pitch,yaw estimation

--- examples/ex_cnn_headp_pose_estimation_images/ex_cnn_head_pose_estimation_images.py
+++ examples/ex_cnn_headp_pose_estimation_images/ex_cnn_head_pose_estimation_images.py
@@ -15,12 +15,19 @@ from deepgaze.head_pose_estimation import CnnHeadPoseEstimator
 
 sess = tf.Session() #Launch the graph in a session.
 my_head_pose_estimator = CnnHeadPoseEstimator(sess) #Head pose estimation object
+
+# Load the weights from the configuration folders
+my_head_pose_estimator.load_roll_variables(os.path.realpath("../../etc/tensorflow/head_pose/roll/cnn_cccdd_30k.tf"))
+my_head_pose_estimator.load_pitch_variables(os.path.realpath("../../etc/tensorflow/head_pose/pitch/cnn_cccdd_30k.tf"))
 my_head_pose_estimator.load_yaw_variables(os.path.realpath("../../etc/tensorflow/head_pose/yaw/cnn_cccdd_30k"))
 
 for i in range(1,9):
     file_name = str(i) + ".jpg"
     print("Processing image ..... " + file_name)
     image = cv2.imread(file_name) #Read the image with OpenCV
-    yaw = my_head_pose_estimator.return_yaw(image) #Evaluate the yaw angle using a CNN
-    print("Estimated yaw ..... " + str(yaw[0,0,0]))
+    # Get the angles for roll, pitch and yaw
+    roll = my_head_pose_estimator.return_roll(image)  # Evaluate the roll angle using a CNN
+    pitch = my_head_pose_estimator.return_pitch(image)  # Evaluate the pitch angle using a CNN
+    yaw = my_head_pose_estimator.return_yaw(image)  # Evaluate the yaw angle using a CNN
+    print("Estimated [roll, pitch, yaw] ..... [" + str(roll[0,0,0]) + "," + str(pitch[0,0,0]) + "," + str(yaw[0,0,0])  + "]")
     print("")
