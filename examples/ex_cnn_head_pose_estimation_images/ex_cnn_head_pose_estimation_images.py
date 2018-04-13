34a43d28c06fad1ccc25623570f5c40504f2ae21
Massimiliano Patacchiola <massimiliano.patacchiola@gmail.com>
2018-04-13 10:23:41 +0100

    Fixing wrong path for yaw weights

--- examples/ex_cnn_head_pose_estimation_images/ex_cnn_head_pose_estimation_images.py
+++ examples/ex_cnn_head_pose_estimation_images/ex_cnn_head_pose_estimation_images.py
@@ -19,7 +19,7 @@ my_head_pose_estimator = CnnHeadPoseEstimator(sess) #Head pose estimation object
 # Load the weights from the configuration folders
 my_head_pose_estimator.load_roll_variables(os.path.realpath("../../etc/tensorflow/head_pose/roll/cnn_cccdd_30k.tf"))
 my_head_pose_estimator.load_pitch_variables(os.path.realpath("../../etc/tensorflow/head_pose/pitch/cnn_cccdd_30k.tf"))
-my_head_pose_estimator.load_yaw_variables(os.path.realpath("../../etc/tensorflow/head_pose/yaw/cnn_cccdd_30k"))
+my_head_pose_estimator.load_yaw_variables(os.path.realpath("../../etc/tensorflow/head_pose/yaw/cnn_cccdd_30k.tf"))
 
 for i in range(1,9):
     file_name = str(i) + ".jpg"
